c194abf1bf67f30eabee0847d1161de54b6bb817
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-08-26 12:49:57 +0700

    Remove path alias, add tsconfig for building scripts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * as constants from '@constants/_index.js';
-export * as schemas from '@schemas/_index.js';
-export * as utils from '@utils/_index.js';
+export * as constants from './constants/_index.js';
+export * as schemas from './schemas/_index.js';
+export * as utils from './utils/_index.js';
