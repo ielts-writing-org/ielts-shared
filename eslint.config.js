b1019cca35a647155b073c7c88e4518338e1114c
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-08-30 13:29:25 +0700

    Refactor configuration files: update .npmignore, add .prettierignore, modify eslint and tsconfig settings, and replace vitest.config.ts with vitest.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
+import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-	globalIgnores(['node_modules/**', 'dist/**', 'eslint.config.js']),
+	globalIgnores(['node_modules/**', 'dist/**', 'eslint.config.js', 'vitest.config.js']),
+	js.configs.recommended,
 	...tseslint.configs.strictTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
 	{
 		files: ['**/*.ts'],
 		languageOptions: {
